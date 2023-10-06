604c4db0b111db2ef90d7a1edd5dae027928c1dc
okellomarvinkevin <marvin.okello@sophany.it>
2023-10-06 17:01:29 +0200

    feat: updated theme with classes and updated the html

--- src/components/Image/Image.tsx
+++ src/components/Image/Image.tsx
@@ -1,32 +1,123 @@
 // import type {ComponentProps} from "react";
 import type { ComponentProps } from 'react';
-import { forwardRef } from 'react';
+import { forwardRef, useId } from 'react';
+import { twMerge } from 'tailwind-merge';
 import { mergeDeep } from '~/src/helpers/merge-deep';
 import { useTheme, type DeepPartial } from '..';
 
 export interface FlowbiteImageTheme {
-  default: string;
+  figure: any;
+  image: any;
 }
 
 export interface ImageProps extends Omit<ComponentProps<'img'>, any> {
+  ref?: string;
+  caption?: string;
+  imageCard?: boolean;
+  imageCaption?: boolean;
+  borderRadius?: string;
+  fullCircle?: boolean;
+  imageShadow?: boolean;
+  retinaReady?: boolean;
+  grayScale?: boolean;
+  blurLevel?: string;
+  size?: string;
+  alignment?: string;
+  link?: string;
   alt?: string;
   src: string;
   theme?: DeepPartial<FlowbiteImageTheme>;
 }
 
-export const Image = forwardRef<HTMLDivElement, ImageProps>(({ src, alt, theme: customTheme = {}, ...props }, ref) => {
-  const theme = mergeDeep(useTheme().theme.image, customTheme);
-  console.log(theme);
-  console.log({ ...props });
+export const Image = forwardRef<HTMLDivElement, ImageProps>(
+  (
+    {
+      caption,
+      imageCard,
+      imageCaption,
+      borderRadius = null,
+      fullCircle,
+      imageShadow = false,
+      retinaReady = false,
+      grayScale,
+      blurLevel = null,
+      size,
+      link,
+      alignment = 'left',
+      src,
+      alt,
+      theme: customTheme = {},
+      ...props
+    },
+    ref,
+  ) => {
+    const theme = mergeDeep(useTheme().theme.image, customTheme);
+    console.log({ ...props });
+    // here determine which property was passed and if it needs a figure or not
+    const showImageCaption = imageCard === null || imageCaption ? false : true;
+    const id = useId();
 
-  return (
-    <div ref={ref}>
-      <figure className="max-w-lg">
-        <img className="h-auto max-w-full rounded-lg" src={src} alt={alt} />
-        <figcaption className="mt-2 text-center text-sm text-gray-500 dark:text-gray-400">Image caption</figcaption>
-      </figure>
-    </div>
-  );
-});
+    return (
+      <div ref={ref}>
+        {showImageCaption ? (
+          <figure
+            className={twMerge(
+              imageCard ? theme.figure.cardImage.figure : '',
+              imageCaption ? theme.figure.captionImage.figure : '',
+            )}
+          >
+            <a href={link}>
+              <img
+                className={twMerge(
+                  theme.image.base,
+                  borderRadius !== null ? theme.image.radius.borderRadius : '',
+                  fullCircle !== null ? theme.image.fullCircle : '',
+                  imageShadow ? theme.image.shadow : '',
+                  retinaReady ? theme.image.retina : '',
+                  grayScale ? theme.image.effects.gray : '',
+                  blurLevel !== null ? theme.image.effects.blur.blurLevel : '',
+                  alignment !== null ? theme.image.align.alignment : '',
+                  size !== null ? theme.image.sizes.size : '',
+                )}
+                src={src}
+                alt={alt}
+                id={id}
+                data-testid="image-1"
+              />
+            </a>
+            <figcaption
+              className={twMerge(
+                imageCard ? theme.figure.cardImage.figureCaption : '',
+                imageCaption ? theme.figure.captionImage.figureCaption : '',
+              )}
+            >
+              {caption}
+            </figcaption>
+          </figure>
+        ) : (
+          <a href={link}>
+            <img
+              className={twMerge(
+                theme.image.base,
+                borderRadius !== null ? theme.image.radius.borderRadius : '',
+                fullCircle !== null ? theme.image.fullCircle : '',
+                imageShadow ? theme.image.shadow : '',
+                retinaReady ? theme.image.retina : '',
+                grayScale ? theme.image.effects.gray : '',
+                blurLevel !== null ? theme.image.effects.blur.blurLevel : '',
+                alignment !== null ? theme.image.align.alignment : '',
+                size !== null ? theme.image.sizes.size : '',
+              )}
+              src={src}
+              alt={alt}
+              id={id}
+              data-testid="image-2"
+            />
+          </a>
+        )}
+      </div>
+    );
+  },
+);
 
 Image.displayName = 'Image';
--- src/components/Image/theme.ts
+++ src/components/Image/theme.ts
@@ -1,5 +1,44 @@
 import type { FlowbiteImageTheme } from './Image';
 
 export const imageTheme: FlowbiteImageTheme = {
-  default: '',
+  figure: {
+    imageCard: {
+      figure: 'relative max-w-sm transition-all duration-300 cursor-pointer filter grayscale hover:grayscale-0',
+      figureCaption: 'absolute px-4 text-lg text-white bottom-6',
+    },
+    imageCaption: {
+      figure: 'max-w-lg',
+      figureCaption: 'mt-2 text-sm text-center text-gray-500 dark:text-gray-400',
+    },
+  },
+  image: {
+    base: 'h-auto max-w-full',
+    borderRadius: {
+      sm: 'rounded-sm',
+      md: 'rounded-md',
+      lg: 'rounded-lg',
+    },
+    fullCircle: 'rounded-full',
+    imageShadow: 'shadow-xl dark:shadow-gray-800',
+    retinaReady: 'rounded-lg',
+    effects: {
+      grayScale: 'transition-all duration-300 rounded-lg cursor-pointer filter grayscale hover:grayscale-0',
+      blur: {
+        xs: 'transition-all duration-300 rounded-lg blur-xm hover:blur-none',
+        sm: 'transition-all duration-300 rounded-lg blur-sm hover:blur-none',
+        lg: 'transition-all duration-300 rounded-lg blur-lg hover:blur-none',
+        xl: 'transition-all duration-300 rounded-lg blur-xl hover:blur-none',
+      },
+    },
+    alignment: {
+      left: '', //because the image is on the left by default
+      centre: 'mx-auto',
+      right: 'ml-auto',
+    },
+    sizes: {
+      sm: 'h-auto max-w-xs',
+      md: 'h-auto max-w-md',
+      lg: 'h-auto max-w-xl',
+    },
+  },
 };
